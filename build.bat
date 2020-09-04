891242973e7cbc9c322729319c70941551838a0f
tnt7864 <tnt7864@rit.edu>
2020-09-04 18:02:18 -0400

    move java files to src/main/java and src/test/java

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
-javac -cp lib/junit-4.12.jar;lib/hamcrest-core-1.3.jar *.java
-java -cp .;lib/junit-4.12.jar;lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore EdgeConnectorTest
-java RunEdgeConvert
+javac -cp lib/junit-4.12.jar;lib/hamcrest-core-1.3.jar ./src/*/java/*.java -d build
+java -cp build;lib/junit-4.12.jar;lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore EdgeConnectorTest
+java -cp build RunEdgeConvert
